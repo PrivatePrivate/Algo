e599573316320f80558f2a97f150827c1f7d7e92
Martyna <chomicz.martyna@gmail.com>
2018-02-12 23:28:23 +0100

    initial commit

---
+++ src/main/java/my/algo/sort/ArraySortUtil.java
@@ -0,0 +1,17 @@
+package my.algo.sort;
+
+public class ArraySortUtil {
+	
+	public static int[] bubbleSort(int [] array) {
+		
+	}
+	
+	public static int[] insertionSort(int [] array) {
+		
+	}
+	
+	public static int[] selectionSort(int [] array) {
+		
+	}
+
+}
---
+++ src/test/java/my/algo/sort/ArraySortUtilTest.java
@@ -0,0 +1,90 @@
+package my.algo.sort;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ArraySortUtilTest {
+	
+	
+	//********************* BUBBLE SORT *************************************
+	@Test
+	public void bubbleSortTest1() {
+		int[] testData = {1, 2, 3};
+		int [] result = ArraySortUtil.bubbleSort(testData);
+		int [] expectedResult = {1, 2, 3};
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void bubbleSortTest12() {
+		int[] testData = {543, 89, 0 , -3, 65, 78, 0, 1};
+		int [] result = ArraySortUtil.bubbleSort(testData);
+		int [] expectedResult = {-3, 0, 0, 1, 65, 78, 89, 543};
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void bubbleSortTest3() {
+		int[] testData = {3, 3, 2, 2, 1, 1};
+		int [] result = ArraySortUtil.bubbleSort(testData);
+		int [] expectedResult = {1, 1, 2, 2, 3, 3};
+		assertEquals(expectedResult, result);
+	}
+	
+	//************************ SELECTION SORT **************************************
+	
+	@Test
+	public void selectionSortTest1() {
+		int[] testData = {1, 2, 3};
+		int [] result = ArraySortUtil.selectionSort(testData);
+		int [] expectedResult = {1, 2, 3};
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void selectionSortTest12() {
+		int[] testData = {543, 89, 0 , -3, 65, 78, 0, 1};
+		int [] result = ArraySortUtil.selectionSort(testData);
+		int [] expectedResult = {-3, 0, 0, 1, 65, 78, 89, 543};
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void selectionSortTest3() {
+		int[] testData = {3, 3, 2, 2, 1, 1};
+		int [] result = ArraySortUtil.selectionSort(testData);
+		int [] expectedResult = {1, 1, 2, 2, 3, 3};
+		assertEquals(expectedResult, result);
+	}
+	
+	//*********************** INSERTION SORT *******************************************
+	
+	@Test
+	public void insertionSortTest1() {
+		int[] testData = {1, 2, 3};
+		int [] result = ArraySortUtil.insertionSort(testData);
+		int [] expectedResult = {1, 2, 3};
+		assertEquals(expectedResult, result);
+	}
+
+	@Test
+	public void insertionSortTest12() {
+		int[] testData = {543, 89, 0 , -3, 65, 78, 0, 1};
+		int [] result = ArraySortUtil.insertionSort(testData);
+		int [] expectedResult = {-3, 0, 0, 1, 65, 78, 89, 543};
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void insertionSortTest3() {
+		int[] testData = {3, 3, 2, 2, 1, 1};
+		int [] result = ArraySortUtil.insertionSort(testData);
+		int [] expectedResult = {1, 1, 2, 2, 3, 3};
+		assertEquals(expectedResult, result);
+	}
+	
+	//*************************************************
+
+
+}
